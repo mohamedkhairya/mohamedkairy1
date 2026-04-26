117a6f76a873a90e9a7fa2c828c1265f8c506fb4
mohamedkhairya <mk2016058.gmail.com>
2026-04-27 01:27:58 +0300

    secind commit

--- src/actionsRegister/cart.action.ts
+++ src/actionsRegister/cart.action.ts
@@ -129,37 +129,5 @@ return data
 }
 
 
-export async function addToWishlistAction(productId: string) {
-  const token = await myAddcartToken();
-
-  if (!token) {
-    return { status: "fail", message: "Please login first" };
-  }
-
-  try {
-    const res = await fetch('https://ecommerce.routemisr.com/api/v1/wishlist', {
-      method: "POST",
-      headers: {
-        "token": token as string,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ productId: productId })
-    });
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("Wishlist Error:", error);
-    return { status: "error", message: "Network error" };
-  }
-}
 
 
-export async function removeFromWishlistAction(productId: string) {
-    const token = await myAddcartToken();
-    const res = await fetch(`https://ecommerce.routemisr.com/api/v1/wishlist/${productId}`, {
-        method: "DELETE",
-        headers: { "token": token || "" }
-    });
-    return await res.json();
-}
