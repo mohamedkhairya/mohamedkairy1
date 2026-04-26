22d7a888736b5121bcc059ee2b85bf13d2b189f5
mohamedkhairya <mk2016058.gmail.com>
2026-04-27 02:35:08 +0300

    second commit

--- src/app/_comp-nav/section/ProductSection.tsx
+++ src/app/_comp-nav/section/ProductSection.tsx
@@ -4,12 +4,12 @@ import ProductCart from './../productcart/productcart';
 
 const ProductSection = async () => {
 
-  const pordacts = await getAllPosts() as any[];
+  const pordacts = await getAllPosts();
 
   return (
     <>
       <div className="grid w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-6 p-4">
-        {pordacts?.map((item: any) => { // ضفنا : any هنا كمان للاحتياط
+        {pordacts?.map((item) => {
           return <ProductCart key={item._id} item={item} />
         })}
       </div>
@@ -17,4 +17,4 @@ const ProductSection = async () => {
   )
 }
 
-export default ProductSection;
+export default ProductSection
